16634ef4e03646414a48f75601c8ae281ccdc4f8
yurididomenico <yurididomenico93@gmail.com>
2022-11-21 15:05:16 +0100

    JS: Stampare 10 mail

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,22 +7,27 @@ var app = new Vue
     el: '#app',
     data: 
     {
-        mail: ""
+        listaMail:
+        [
+            
+        ]
     },
 
     methods: 
     {
         test()
         {
+            listaMail = ""
             for(let i=0; i<10; i++)
             {
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then((risposta) =>
                 {
                     let nuovaMail = risposta.data.response
-                    this.mail = nuovaMail
-                    console.log(this.mail)
+                    this.listaMail.push(nuovaMail)
+                    console.log(this.listaMail[i])
                 })
+                
             }
         }
     }
