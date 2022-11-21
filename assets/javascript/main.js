fe3ae942f1249ef51ebf79fb0bd94b43965b3ca3
yurididomenico <yurididomenico93@gmail.com>
2022-11-21 14:11:48 +0100

    Set Workspace

---
+++ assets/javascript/main.js
@@ -0,0 +1,41 @@
+// JAVASCRIPT //
+
+console.log("YDD");
+
+var app = new Vue({
+el: '#app',
+data: {
+
+},
+mounted() {
+
+},
+beforeUpdate() {
+
+},
+methods: {
+
+}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----- FINE :) -----
